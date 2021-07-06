f2fd293d95858998488d4cb33f4e0181b6b90bda
Sundar-Gandu <86447677+Sundar-Gandu@users.noreply.github.com>
2021-07-06 17:55:20 -0400

    Change logger to debug

--- oneclickagility/src/main/java/net/runelite/client/plugins/oneclickagility/ObstacleArea.java
+++ oneclickagility/src/main/java/net/runelite/client/plugins/oneclickagility/ObstacleArea.java
@@ -52,7 +52,7 @@ public class ObstacleArea
     {
         if (nextObstacle != null)
         {
-            log.info(nextObstacle.getActions()[0] + ", " + nextObstacle.getName() + ", " + nextObstacle.getId() + ", " + "MenuAction.GAME_OBJECT_FIRST_OPTION.getId()" + ", " + getObjectParam(nextObstacle) + ", " + getObjectParam1(nextObstacle));
+            log.debug(nextObstacle.getActions()[0] + ", " + nextObstacle.getName() + ", " + nextObstacle.getId() + ", " + "MenuAction.GAME_OBJECT_FIRST_OPTION.getId()" + ", " + getObjectParam(nextObstacle) + ", " + getObjectParam1(nextObstacle));
             return new MenuEntry(nextObstacle.getActions()[0], nextObstacle.getName(),nextObstacle.getId(), MenuAction.GAME_OBJECT_FIRST_OPTION.getId(), getObjectParam(nextObstacle),getObjectParam1(nextObstacle),true);
         }
         return null;
