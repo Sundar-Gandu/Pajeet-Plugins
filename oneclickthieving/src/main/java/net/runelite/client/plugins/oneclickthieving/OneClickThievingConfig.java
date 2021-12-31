2bbbad5f812bbcb2abb54021d9697ce7a29f039b
Foxori <31644437+Foxori@users.noreply.github.com>
2021-12-31 04:10:03 +0200

    Revert "Blackjack fixes for one click thiev"
    This reverts commit 64718145c536528c86b28f0c7a7e2cf805bfa62f.

--- oneclickthieving/src/main/java/net/runelite/client/plugins/oneclickthieving/OneClickThievingConfig.java
+++ oneclickthieving/src/main/java/net/runelite/client/plugins/oneclickthieving/OneClickThievingConfig.java
@@ -4,76 +4,158 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
+import static net.runelite.client.plugins.oneclickthieving.PrayMethod.LAZY_PRAY;
 
 @ConfigGroup("oneclicjthieving")
-public interface OneClickThievingConfig extends Config {
-    @ConfigItem(keyName = "clickOverride", name = "Clicks Override", description = "Makes all of your clicks pickpocket chosen NPC", position = -2)
-    default boolean clickOverride() {
+public interface OneClickThievingConfig extends Config
+{
+    @ConfigItem(
+            keyName = "clickOverride",
+            name = "Clicks Override",
+            description = "Makes all of your clicks pickpocket chosen NPC",
+            position = -2
+    )
+    default boolean clickOverride()
+    {
         return false;
     }
-
-    @ConfigItem(keyName = "npcID", name = "NPC ID", description = "the id of the npc you want to pickpocket", position = -1, hidden = true, unhide = "clickOverride")
-    default int npcID() {
+    @ConfigItem(
+            keyName = "npcID",
+            name = "NPC ID",
+            description = "the id of the npc you want to pickpocket",
+            position = -1,
+            hidden = true,
+            unhide = "clickOverride"
+    )
+    default int npcID()
+    {
         return 0;
     }
-
-    @ConfigItem(keyName = "enableCoinPouch", name = "Open Coin Pouch", description = "Opens the coinpouch when it reaches the maximum of 28", position = 0)
-    default boolean enableCoinPouch() {
+    @ConfigItem(
+            keyName = "enableCoinPouch",
+            name = "Open Coin Pouch",
+            description = "Opens the coinpouch when it reaches the maximum of 28",
+            position = 0
+    )
+    default boolean enableCoinPouch()
+    {
         return true;
     }
 
-    @ConfigItem(keyName = "enableHeal", name = "Eat food", description = "This will eat/drink anything in your inventory when your HP gets low", position = 1)
-    default boolean enableHeal() {
+    @ConfigItem(
+            keyName = "enableHeal",
+            name = "Eat food",
+            description = "This will eat/drink anything in your inventory when your HP gets low",
+            position = 1
+    )
+    default boolean enableHeal()
+    {
         return true;
     }
 
-    @Range(max = 99, min = 5)
-    @ConfigItem(keyName = "HPTopThreshold", name = "Maximum HP", description = "You will STOP eating when your HP is at or above this number", position = 2, hidden = true, unhide = "enableHeal")
-    default int HPTopThreshold() {
+    @Range(
+            max = 99,
+            min = 5
+    )
+    @ConfigItem(
+            keyName = "HPTopThreshold",
+            name = "Maximum HP",
+            description = "You will STOP eating when your HP is at or above this number",
+            position = 2,
+            hidden = true,
+            unhide = "enableHeal"
+    )
+    default int HPTopThreshold()
+    {
         return 80;
     }
 
-    @Range(max = 99, min = 5)
-    @ConfigItem(keyName = "HPBottomThreshold", name = "Minimum HP", description = "You will START eating when your HP is at or below this number", position = 3, hidden = true, unhide = "enableHeal")
-    default int HPBottomThreshold() {
+    @Range(
+            max = 99,
+            min = 5
+    )
+    @ConfigItem(
+            keyName = "HPBottomThreshold",
+            name = "Minimum HP",
+            description = "You will START eating when your HP is at or below this number",
+            position = 3,
+            hidden = true,
+            unhide = "enableHeal"
+    )
+    default int HPBottomThreshold()
+    {
         return 5;
     }
 
-    @ConfigItem(keyName = "haltOnNoFood", name = "Stop on No Food/Prayer", description = "This will notify you and prevent you from picpocketing when you run out of food or prayer potions", position = 4, hidden = true, unhide = "enableHeal")
-    default boolean haltOnLowFood() {
+    @ConfigItem(
+            keyName = "haltOnNoFood",
+            name = "Stop on No Food/Prayer",
+            description = "This will notify you and prevent you from picpocketing when you run out of food or prayer potions",
+            position = 4,
+            hidden = true,
+            unhide = "enableHeal"
+    )
+    default boolean haltOnLowFood()
+    {
         return true;
     }
 
-    @ConfigItem(keyName = "enablePray", name = "Use Prayer", description = "This will use redemption with prayer/restore potions<br>This is mainly for pickpocketing elves/vyres", position = 5)
-    default boolean enablePray() {
+    @ConfigItem(
+            keyName = "enablePray",
+            name = "Use Prayer",
+            description = "This will use redemption with prayer/restore potions" +
+                    "<br>This is mainly for pickpocketing elves/vyres",
+            position = 5
+    )
+    default boolean enablePray()
+    {
         return false;
     }
 
-    @ConfigItem(keyName = "prayMethod", name = "Pray Method", description = "Lazy reactivates redemption when possible. Active only enables when your hp is low<br>Active should only be used if your max hp is greater than 60", position = 6, hidden = true, unhide = "enablePray")
-    default PrayMethod prayMethod() {
-        return PrayMethod.LAZY_PRAY;
+    @ConfigItem(
+            keyName = "prayMethod",
+            name = "Pray Method",
+            description = "Lazy reactivates redemption when possible. Active only enables when your hp is low" +
+                    "<br>Active should only be used if your max hp is greater than 60",
+            position = 6,
+            hidden = true,
+            unhide = "enablePray"
+    )
+    default PrayMethod prayMethod()
+    {
+        return LAZY_PRAY;
     }
 
-    @ConfigItem(keyName = "enableSpell", name = "Use Shadow Veil", description = "This will cast the arceuus spell Shadow Veil", position = 7)
-    default boolean enableSpell() {
+    @ConfigItem(
+            keyName = "enableSpell",
+            name = "Use Shadow Veil",
+            description = "This will cast the arceuus spell Shadow Veil",
+            position = 7
+    )
+    default boolean enableSpell()
+    {
         return false;
     }
 
-    @ConfigItem(keyName = "enableNecklace", name = "Dodgy Necklace", description = "This will put on dodgy necklaces when they break", position = 8)
-    default boolean enableNecklace() {
+    @ConfigItem(
+            keyName = "enableNecklace",
+            name = "Dodgy Necklace",
+            description = "This will put on dodgy necklaces when they break",
+            position = 8
+    )
+    default boolean enableNecklace()
+    {
         return true;
     }
 
-    @ConfigItem(keyName = "disableWalk", name = "Disable Walk", description = "This will prevent you from misclicking Walk Here", position = 9)
-    default boolean disableWalk() {
-        return false;
-    }
-    @ConfigItem(keyName = "blackjackFix", name = "Blackjack Fix", description = "This will try to prevent you from dying while left click blackjacking", position = 10)
-    default boolean enableBlackjackFix() {
-        return false;
-    }
-    @ConfigItem(keyName = "blackjackResetFood", name = "Blackjack reset food", description = "This is used in blackjacking only. Toggle on and back off when you have restocked food.", position = 11)
-    default boolean BlackjackResetFood() {
+    @ConfigItem(
+            keyName = "disableWalk",
+            name = "Disable Walk",
+            description = "This will prevent you from misclicking Walk Here",
+            position = 9
+    )
+    default boolean disableWalk()
+    {
         return false;
     }
 }
